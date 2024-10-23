a65f33d0737878b7443b68ef820243760bd4e1ba
Ian Brown <bigianb@gmail.com>
2024-10-23 18:15:52 +0100

    tests

--- a51lib/ResourceManager.h
+++ a51lib/ResourceManager.h
@@ -1,2 +1,54 @@
 #pragma once
 
+class ResourceManager;
+
+/**
+ * A handle to a resource in the ResourceManager.
+ * The type of the resource is unknown. Sub-classes are templetised to concrete types.
+ * It needs to know the ResourceManager, so call setResourceManager before setName.
+ */
+class ResourceHandleBase
+{
+public:
+    ResourceHandleBase() {data = -1; resourceManager = nullptr;}
+
+    ~ResourceHandleBase() {}
+
+    void setResourceManager(ResourceManager* mgr) {resourceManager = mgr;}
+
+    void        setName(const char* resourceName);
+    const char* getName() const;
+
+    void* getPointer() const;
+    bool isLoaded() const;
+
+    int getIndex() const { return data; };
+
+    void  destroy();
+    bool isNull() const
+    {
+        return data == -1;
+    }
+
+protected:
+    void setIndex(int idx) { data = idx; }
+
+    int data;
+    ResourceManager* resourceManager;
+};
+
+/**
+ * A specialised ResourceHandleBase.
+ */
+template< class T >
+struct ResourceHandle : public ResourceHandleBase
+{
+    T*  GetPointer  () const;
+};
+
+/**
+ * The ResourceManager owns the resources. A resource is identified by a ResourceHandle.
+ */
+class ResourceManager
+{
+};
--- a51lib_test/ResourceManagerTest.cpp
+++ a51lib_test/ResourceManagerTest.cpp
@@ -1,5 +1,20 @@
 #include <gtest/gtest.h>
 
+#include "../a51lib/ResourceManager.h"
+
+TEST(ResourceManagerTest, BasicOperation) {
+
+  ResourceManager resourceManager;
+
+  // General operation is to have a resource handle,
+  // set the name and then get the pointer.
+
+  ResourceHandleBase baseHandle;
+  baseHandle.setResourceManager(&resourceManager);
+  baseHandle.setName("myResourceName");
+
+}
+
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
   // Expect two strings not to be equal.
