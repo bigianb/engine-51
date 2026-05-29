f3a38ca3ff78704ccb88c4f61c10f43033f2ee4a
Ian Brown <bigianb@gmail.com>
2026-05-29 21:16:04 +0100

    Update ConfigureWindows.bat

--- ConfigureWindows.bat
+++ ConfigureWindows.bat
@@ -1,5 +1,5 @@
 @echo off
-mkdir build_vs2022
-pushd build_vs2022
-cmake -G "Visual Studio 17 2022" -A x64 -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DCMAKE_PREFIX_PATH=C:\Qt\6.8.0\msvc2022_64 ..
+mkdir build_vs2026
+pushd build_vs2026
+cmake -G "Visual Studio 18 2026" -A x64 -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" -DCMAKE_PREFIX_PATH=C:\Qt\6.11.1\msvc2022_64 ..
 popd
